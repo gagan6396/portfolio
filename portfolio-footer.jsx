1145920d16ad9c622455444deb7640c3784ec61f
Arun2880 <a06050r@gmail.com>
2026-08-18 17:22:30 -0400

    linked in change

--- portfolio-footer.jsx
+++ portfolio-footer.jsx
@@ -93,7 +93,7 @@ const PortfolioFooter = () => (
             </svg>
             +91 9506050288
           </FooterLink>
-          <FooterLink href="https://linkedin.com/in/arunkumar" isExternal>
+          <FooterLink href="www.linkedin.com/in/arun-kumar-374505234" isExternal>
             <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" style={{ marginRight: 8, verticalAlign: 'middle' }}>
               <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/>
               <rect x="2" y="9" width="4" height="12"/>
